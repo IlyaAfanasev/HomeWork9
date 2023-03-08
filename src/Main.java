8527a28217951cd795d1aff5667a42abacf7980a
IlyuAfanasev <gecktor.86@mail.ru>
2023-03-08 16:43:41 +0400

    task 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static int[] generateRandomArray() {
@@ -13,17 +14,18 @@ public class Main {
         }
         return arr;
     }
-
     public static void task1() {
+        System.out.println("     Задача 1");
         int[] arr = generateRandomArray();
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
         }
-        System.out.println(sum);
+        System.out.println("Сумма трат за месяц составила-" +sum+" рублей");
     }
 
     public static void task2() {
+        System.out.println("     Задача 2");
         int[] arr = generateRandomArray();
         int max = 0;
         int min = 200_000;
@@ -38,5 +40,14 @@ public class Main {
         }
         System.out.println("Максимальная сумма трат за день составила-" + max + " рублей");
         System.out.println("Минимальная сумма трат за день составила-" + min + " рублей");
+    }
+    public static void task3() {
+        System.out.println("     Задача 3");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println("Средняя сумма трат за месяц составила-" +sum/ arr.length+" рублей");
     }
 }
