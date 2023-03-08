d445fe9077d216a3bc9f85ed5f26d70f4452b483
IlyuAfanasev <gecktor.86@mail.ru>
2023-03-08 16:08:45 +0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        task1();
     }
         public static int[] generateRandomArray() {
             java.util.Random random = new java.util.Random();
@@ -10,5 +10,12 @@ public class Main {
             }
             return arr;
         }
-
+    public static void task1() {
+        int[] arr = generateRandomArray();
+        int sum=0;
+        for (int i = 0; i < arr.length; i++) {
+            sum+=arr[i];
+        }
+        System.out.println(sum);
+    }
 }
